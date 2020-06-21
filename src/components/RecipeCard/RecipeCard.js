8fa9854b554506a3275129f38a18640dd8851752
namita.shenai@gmail.com <namita.shenai@gmail.com>
2020-06-21 15:37:51 -0700

    Updated styling related to recipe card

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,26 +1,38 @@
 import React from "react";
-import { Icon, Button } from 'semantic-ui-react'; 
-import  "./RecipeCard.css"
+import { Icon, Button } from 'semantic-ui-react';
+import "./RecipeCard.css"
 
 
 const RecipeCard = (props) => {
     return (
-        
-            <div className="recipe-card">
-                <h3 className="recipe-label">{props.title}</h3>
+
+        <div className="recipe-card">
+            <h3 className="recipe-label">{props.title}</h3>
+            <div className="row">
                 <img src={props.image} alt={props.label} className="round-image col-md-4" />
-                <br/>
-                <div className="col-md-8 recipe-info">
+                <br />
+                <div className="col-md-6 recipe-info">
                     <a href={props.url} target="_blank" rel="noopener noreferrer">GET RECIPE!</a>
                     <br />
                     <h3>Yields: {props.yield}</h3>
                     <h3>Source: {props.source}</h3>
-                    <h5> Health Label(s): {props.healthLabels.map((label, i)=>{
+                    <h5> Health Label(s): {props.healthLabels.map((label, i) => {
                         return <span id={i}> {label} / </span>
                     })} </h5>
+                    <span>Ingredients :{props.ingredients.map((ingredient, i) => {
+                        // console.log("ingredient: ", ingredient.text);
+                        return (
 
+                            <li key={i} className="ingredient">
+                                {ingredient.text}
+                            </li>
+
+                        );
+                    })}
+                    </span>
                 </div>
             </div>
+        </div>
 
 
 
