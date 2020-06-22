31513efdb2350c0c39a4280685fbe0fa1a4ab0dc
namita.shenai@gmail.com <namita.shenai@gmail.com>
2020-06-21 20:03:54 -0700

    fixed server in packae.json

---
+++ build/precache-manifest.a82613fdd7d1b4dd66c697c8e128691f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25f06aa08ce97c3c961cbcfb08605dda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b2bb1a0ed3fe7a8fbd0",
+    "url": "/static/css/main.c0b29fc4.chunk.css"
+  },
+  {
+    "revision": "3dc85d68e103ff85cfbc",
+    "url": "/static/js/2.807c59d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.807c59d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2bb1a0ed3fe7a8fbd0",
+    "url": "/static/js/main.73852653.chunk.js"
+  },
+  {
+    "revision": "911a39a17e3afcb47ec2",
+    "url": "/static/js/runtime-main.1f4ee23f.js"
+  }
+]);
